3b80b697fb10015ca9d71ee1b63f40b347d12478
AndriiDevX <sapsayandrey1@gmail.com>
2026-05-24 18:10:38 +0300

    chore: successfully initialize firebase core and auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pizzeria/data/app_router.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(ProviderScope(child: MainApp()));
 }
 
