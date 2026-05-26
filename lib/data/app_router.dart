47441387fa1fda88cf1cef63e0e30783e084d4c5
AndriiDevX <sapsayandrey1@gmail.com>
2026-05-26 17:09:16 +0300

    feat: add automatic route redirect based on auth state

--- lib/data/app_router.dart
+++ lib/data/app_router.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:go_router/go_router.dart';
 import 'package:pizzeria/screens/home_screen.dart';
 import 'package:pizzeria/screens/cart_screen.dart';
@@ -5,10 +6,12 @@ import 'package:pizzeria/screens/order_screen.dart';
 import 'package:pizzeria/screens/login_screen.dart';
 
 final router = GoRouter(
+  initialLocation: FirebaseAuth.instance.currentUser != null ? '/home' :'/',
   routes: [
     GoRoute(path: '/', builder: (context, state) => const LoginScreen()),
     GoRoute(path: '/home', builder: (context, state) => const HomeScreen()),
     GoRoute(path: '/cart', builder: (context, state) => const CartScreen()),
     GoRoute(path: '/order', builder: (context, state) => const OrderScreen()),
+   
   ],
 );
